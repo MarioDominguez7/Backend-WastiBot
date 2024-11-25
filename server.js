ad3b292d3f0f77dbb5f7685d1511c7a7445e0e15
SamyAlinne <117871370+SamyAlinne@users.noreply.github.com>
2024-11-24 19:42:54 -0600

    Más palabras clave para cada cosa

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/* --------------------------
+#  DECLARACION DE CONSTANTES
+----------------------------- */
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -29,29 +32,68 @@ app.post('/api/chat', (req, res) => {
   let movieQuery = null;
   let queryParams = [];
   let movieContext = null;
+  let conversationEnded = false;
 
-  // Detectar agradecimiento o despedida
+  // Detectar saludos, agradecimiento o despedida
+  if (conversationEnded) {
+    if (
+      userMessage.includes('hola') ||
+      userMessage.includes('saludos') ||
+      userMessage.includes('holi') ||
+      userMessage.includes('ola') ||
+      userMessage.includes('holiwis')
+    ) {
+      conversationEnded = false; 
+      botResponse = '¡Hola! Soy WastiBot, tu asistente para películas navideñas. ¿En qué puedo ayudarte hoy?';
+    } else {
+      botResponse = '¿Sigues ahí? :o ¿Quieres saber sobre alguna otra película?';
+    }
+    return res.json({ message: botResponse, endConversation: false });
+  }
+  
+  if (
+    userMessage.includes('hola') ||
+    userMessage.includes('saludos') ||
+    userMessage.includes('holi') ||
+    userMessage.includes('ola') ||
+    userMessage.includes('holiwis')
+  ) {
+    botResponse = '¡Hola de nuevo! ¿Te puedo ayudar con algo más sobre películas navideñas?';
+    return res.json({ message: botResponse, endConversation: false });
+  }
+
   if (userMessage.includes('muchas gracias') || userMessage.includes('gracias')) {
     currentMovie = null;
     botResponse = 'Me alegro de haber sido de ayuda. ¿Quieres que te hable de otra película navideña?';
     return res.json({ message: botResponse, endConversation: false });
-  } else if (userMessage.includes('adios') || userMessage.includes('adiós') || userMessage.includes('hasta luego')) {
-    botResponse = '¡Hasta luego! Espero que hayas disfrutado hablando sobre películas navideñas. ¡Vuelve pronto!';
+  } else if (userMessage.includes('adios') || userMessage.includes('adiós') || userMessage.includes('hasta luego')) { 
+    conversationEnded = true;
+    botResponse = '¡Hasta luego! Espero que hayas disfrutado nuestra conversación sobre películas navideñas. <br>¡Vuelve pronto! ヾ(￣▽￣)';
     return res.json({ message: botResponse, endConversation: true });
   }
 
-  if (userMessage.includes('mejor película') || userMessage.includes('top película') || userMessage.includes('mejor pelicula') || userMessage.includes('mejor calificada') 
-    || userMessage.includes('mayor calificacion') || userMessage.includes('mayor calificación')) {
+  // Si el usuario quiere hablar de otra película
+  if (userMessage.includes('de otra') || userMessage.includes('otra película') || userMessage.includes('otra pelicula') || userMessage.includes('segunda opción') || userMessage.includes('segunda opcion') || userMessage.includes('la 2') || userMessage.includes('la segunda')  || userMessage.includes('la dos') || userMessage.includes('cambiemos') || userMessage.includes('cambia')) {
+    currentMovie = null; 
+    botResponse = 'Claro, cambiemos de tema. ¿Quieres que te recomiende una película o tienes alguna en mente?';
+    return res.json({ message: botResponse, endConversation: false });
+  } 
+
+  // Iniciar para recomendar película
+  if (userMessage.includes('mejor película') || userMessage.includes('top película') || userMessage.includes('mejor pelicula') || userMessage.includes('mejor calificada') || userMessage.includes('top pelicula')
+    || userMessage.includes('mayor calificacion') || userMessage.includes('mayor calificación') || userMessage.includes('mayor rating') || userMessage.includes('top 1') || userMessage.includes('top uno')) {
     movieQuery = 'SELECT * FROM peliculas ORDER BY calificacion DESC LIMIT 1';
     movieContext = 'mejor';
     botResponse = 'La mejor película navideña según nuestros datos es:';
-  } else if (userMessage.includes('recomendar') || userMessage.includes('sugerir') || userMessage.includes('sugiereme') || userMessage.includes('sugiéreme') 
-    || userMessage.includes('recomiéndame') || userMessage.includes('recomiendame')) {
+  } else if (userMessage.includes('recomendar') || userMessage.includes('sugerir') || userMessage.includes('sugieres') || userMessage.includes('sugiereme') || userMessage.includes('sugiéreme') 
+    || userMessage.includes('recomiéndame') || userMessage.includes('recomiendame') || userMessage.includes('recomiendas') || userMessage.includes('puedo ver') || userMessage.includes('dime otra') 
+    || userMessage.includes('dime una') || userMessage.includes('dime 1') || userMessage.includes('dime otra') || userMessage.includes('sugiere')) {
     movieQuery = 'SELECT * FROM peliculas ORDER BY RAND() LIMIT 1';
     movieContext = 'recomendación';
     botResponse = 'Te recomiendo ver:';
-  } else if (userMessage.includes('peor película') || userMessage.includes('peor calificada') || userMessage.includes('peor pelicula')
-    || userMessage.includes('menor calificacion') || userMessage.includes('menor calificación')) {
+  } else if (userMessage.includes('peor película') || userMessage.includes('top película') || userMessage.includes('peor calificada') || userMessage.includes('peor pelicula')
+    || userMessage.includes('menor calificacion') || userMessage.includes('menor calificación') || userMessage.includes('menor rating') || userMessage.includes('más baja') || userMessage.includes('más bajo')
+    || userMessage.includes('mas baja') || userMessage.includes('mas bajo')) {
     movieQuery = 'SELECT * FROM peliculas ORDER BY calificacion ASC LIMIT 1';
     movieContext = 'peor';
     botResponse = 'La película navideña con menor calificación según nuestros datos es:';
@@ -91,7 +133,7 @@ app.post('/api/chat', (req, res) => {
             break;
         }
         
-        botResponse += ` ¿Qué te gustaría saber sobre esta película? Puedes preguntar sobre el año, director, calificación, género, trama o dónde verla.`;
+        botResponse += ` <br> ¿Qué te gustaría saber sobre esta película? Puedes preguntar sobre el año, director, calificación, género, trama o dónde verla.`;
         
         res.json({ 
           message: botResponse, 
@@ -120,15 +162,15 @@ app.post('/api/chat', (req, res) => {
 
 function handleMovieInfoRequest(userMessage, res) {
   let infoType = 'general';
-  if (userMessage.includes('año') || userMessage.includes('cuando')) {
+  if (userMessage.includes('año') || userMessage.includes('cuando') || userMessage.includes('cuándo')) {
     infoType = 'año';
-  } else if (userMessage.includes('director')) {
+  } else if (userMessage.includes('director') || userMessage.includes('dirigió') || userMessage.includes('dirigio')) {
     infoType = 'director';
-  } else if (userMessage.includes('calificación') || userMessage.includes('rating')) {
+  } else if (userMessage.includes('calificación') || userMessage.includes('calificacion') || userMessage.includes('rating')) {
     infoType = 'calificación';
-  } else if (userMessage.includes('género') || userMessage.includes('tipo')) {
+  } else if (userMessage.includes('género') || userMessage.includes('genero') || userMessage.includes('tipo')) {
     infoType = 'género';
-  } else if (userMessage.includes('dónde ver') || userMessage.includes('donde ver') || userMessage.includes('plataformas')) {
+  } else if (userMessage.includes('dónde ver') || userMessage.includes('donde ver') || userMessage.includes('plataformas') || userMessage.includes('plataforma') || userMessage.includes('streaming') || userMessage.includes('donde la puedo ver') || userMessage.includes('dónde la puedo ver') || userMessage.includes('donde puedo verla') || userMessage.includes('dónde puedo verla')) {
     infoType = 'donde_ver';
   }
 
@@ -160,33 +202,15 @@ function handleMovieInfoRequest(userMessage, res) {
           break;
           case 'donde_ver':
             if (movie.donde_ver) {
-              // Convierte la lista de plataformas y enlaces en HTML con hipervínculos
-              const plataformas = movie.donde_ver
-                .split('\n') // Si los datos están separados por saltos de línea
-                .map(plataforma => {
-                  const [nombre, url] = plataforma.split(': '); // Divide el nombre y el link
-                  return `<a href="${url.trim()}" target="_blank">${nombre.trim()}</a>`; // Formatea como hipervínculo
-                })
-                .join(', <br>'); // Une las plataformas con comas
-              response = `"<b>${movie.titulo}</b>" está disponible en las siguientes plataformas: <br>${plataformas}.`;
+              response = `"<b>${movie.titulo}</b>" está disponible en las siguientes plataformas: <br>${formatPlatforms( movie.donde_ver )}.`;
             } else {
               response = `"<b>${movie.titulo}</b>" no tiene información sobre dónde verla actualmente.`;
             }
             break;
         default:
-          // Divide la información de las plataformas en un array 
-          const platforms = movie.donde_ver.split('\n'); 
-          
-          // Crea una lista formateada con hipervínculos
-          const formattedPlatforms = platforms.map((platform) => {
-            const [name, url] = platform.split(': '); // Divide entre nombre de la plataforma y el link
-            return `<a href="${url}" target="_blank">${name}</a>`;
-          }).join(', <br>'); // Separa cada plataforma con un salto de línea
-          
-          // Genera la respuesta con la lista de plataformas formateadas
           response = `"<b>${movie.titulo}</b>" es una película navideña del año ${movie.anio}, dirigida por ${movie.director}. 
                       Tiene una calificación de ${movie.calificacion} sobre 10 y pertenece al subgénero de ${movie.subgenero}. 
-                      Puedes verla en las siguientes plataformas:<br>${formattedPlatforms}. 
+                      Puedes verla en las siguientes plataformas:<br>${formatPlatforms( movie.donde_ver )}. 
                       <br> ¿Qué más te gustaría saber sobre ella?`;
       }
       response += ' <br>¿Quieres saber algo más sobre esta película o prefieres que hablemos de otra?';
@@ -195,6 +219,17 @@ function handleMovieInfoRequest(userMessage, res) {
       res.json({ message: 'Lo siento, no pude encontrar información sobre esa película.' });
     }
   });
+}
+
+// Función para formatear plataformas y agregarlas como links
+function formatPlatforms(donde_ver) {
+  return donde_ver
+    .split("\n")
+    .map((platform) => {
+      const [name, url] = platform.split(": ");
+      return `<a href="${url}" target="_blank">${name}</a>`;
+    })
+    .join(", <br>");
 }
 
 function generateGenericResponse(message) {
